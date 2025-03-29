fb0dc66ebfbd990b9cbdae508b6258422490d721
Hakimjon <normatovhakimjon07@gmail.com>
2025-03-29 15:05:12 +0500

    add Project

---
+++ src/test/java/org/example/moreeduceorigin/MoreEduceOriginApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.moreeduceorigin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoreEduceOriginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
