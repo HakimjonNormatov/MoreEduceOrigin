ff72fc770be4fc710f34705dff29622c3a82b1cc
Hakimjon <normatovhakimjon07@gmail.com>
2025-03-29 15:05:12 +0500

    address added

---
+++ src/main/java/org/example/moreeduceorigin/dto/AdressDto.java
@@ -0,0 +1,4 @@
+package org.example.moreeduceorigin.dto;
+
+public class AdressDto {
+}
---
+++ src/main/java/org/example/moreeduceorigin/model/Adress.java
@@ -0,0 +1,20 @@
+package org.example.moreeduceorigin.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Adress {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String street;
+}
