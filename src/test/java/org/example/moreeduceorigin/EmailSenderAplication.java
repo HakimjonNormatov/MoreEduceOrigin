e8843899b4e2f93a705f39a1016cc0369096ccdb
spektr035 <muhammadqodirmahmudjonov93@gmail.com>
2025-05-06 09:18:19 +0500

    Email sender added

---
+++ src/test/java/org/example/moreeduceorigin/EmailSenderAplication.java
@@ -0,0 +1,11 @@
+package org.example.moreeduceorigin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EmailSenderAplication {
+
+    @Test
+    void contextLoads() {}
+}
